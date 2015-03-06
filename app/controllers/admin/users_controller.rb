a27af6e7bcadee55749ac5394810968cf3e25560
roryg11 <Rory.C.Grant@gmail.com>
2015-03-06 11:14:01 -0800

    users can add fellows and those fellows show up on an index page

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,24 @@
+class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params.merge({password: "12345678", password_confirmation: "12345678"}))
+    if @user.save
+      redirect_to admin_users_path, notice: "Fellow successfully saved."
+    else
+      render :new, alert: "Fellow could not be saved."
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+end
