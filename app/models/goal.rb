9ad7f2758470eab063f9b9e0bcd774d92e5c310f
roryg11 <Rory.C.Grant@gmail.com>
2015-03-13 14:31:52 -0700

    adding validations to goals and error message

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,11 @@
 class Goal < ActiveRecord::Base
+  validates :due_date, presence: true
+  validate :due_date_cannot_be_in_the_past
   belongs_to :user
+
+  def due_date_cannot_be_in_the_past
+    if due_date.present? && due_date < Date.today
+      errors.add(:due_date, "can't be in the past")
+    end
+  end
 end
