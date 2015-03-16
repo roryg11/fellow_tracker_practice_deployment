ceec79ba0acdbbca530c65bf2391365371f70579
roryg11 <Rory.C.Grant@gmail.com>
2015-03-16 14:59:02 -0700

    User can see all goals in goal history

--- app/controllers/admin/goals_controller.rb
+++ app/controllers/admin/goals_controller.rb
@@ -8,6 +8,18 @@ class Admin::GoalsController < ApplicationController
     @cohort_start_date = @user.cohort.start_date
     @week_number = 1
     @first_monday = @user.cohort.first_monday
+    @cohort_phase_array = []
+    52.times do
+      week_hash = {}
+      week_hash[:week_number] = @week_number
+      week_hash[:start] = (@first_monday + (7*(@week_number -1))).strftime("%B %d %Y")
+      week_hash[:end] = (@first_monday + (7*(@week_number -1)) + 6).strftime("%B %d %Y")
+      week_hash[:goals] = []
+      week_hash[:goals] = @goals.where(due_date: week_hash[:start]..week_hash[:end])
+      @cohort_phase_array.push(week_hash)
+      @week_number+=1
+     end
+    p @cohort_phase_array
   end
 
   def new
