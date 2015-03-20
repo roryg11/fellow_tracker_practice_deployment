ac7f14967cdc1f8bef7ff6d47f70d514545007e3
John Foley <john@hisfoleyness.com>
2015-03-20 09:38:11 -0700

    add a seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+staff = Staff.create!(
+  email: 'admin@uncollege.org',
+  password: 'abcd1234',
+  password_confirmation: 'abcd1234',
+)
+
+fellow = Fellow.create!(
+  email: 'fellow@uncollege.org',
+  password: 'abcd1234',
+  password_confirmation: 'abcd1234',
+)
+
+cohort = Cohort.create!(
+  season: 'Spring',
+  year: '2015',
+  start_date: Date.parse('2015-03-23')
+)
+
+cohort.users << staff
+cohort.users << fellow
