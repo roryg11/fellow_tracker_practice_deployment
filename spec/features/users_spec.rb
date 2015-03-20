fa7c536b405ec592bc9c2245fd3aa4a9ef2b1fe8
John Foley <john@hisfoleyness.com>
2015-03-20 10:04:08 -0700

    fix spec after partial extraction

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -21,8 +21,9 @@ feature "Users" do
     fill_in "Email", with: "rory.c.grant@gmail.com"
     fill_in "Password", with: "roryrocks"
     click_on "Log in"
-    visit root_path
-    click_on 'new-fellow-button'
+
+    click_on 'show-fellows-action'
+    click_on 'new-fellow-action'
     fill_in "First name", with: "Tina"
     fill_in "Last name", with: "Loh"
     fill_in "Email", with: "tinalola@gmail.com"
