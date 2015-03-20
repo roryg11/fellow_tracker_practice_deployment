333a206fc07ee0ebd2e813865517fb3f5da816af
roryg11 <Rory.C.Grant@gmail.com>
2015-03-20 11:50:26 -0700

    fixing users controller syntax error

--- app/controllers/staff/users_controller.rb
+++ app/controllers/staff/users_controller.rb
@@ -1,52 +1,50 @@
 class Staff::UsersController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def index
-      @user = User.all
-    end
+  def index
+    @user = User.all
+  end
 
-    def new
-      @staff = user.new
-    end
+  def new
+    @staff = user.new
+  end
 
-    def create
-      @user = user.new(user_params.merge({password: "12345678", password_confirmation: "12345678"}))
-      if @user.save
-        redirect_to staff_users_path, notice: "user successfully saved."
-      else
-        render :new, alert: "user could not be saved."
-      end
-    end
-
-    def edit
-      @user = user.find(params[:id])
-    end
-
-    def update
-      @user = user.find(params[:id])
-      @user.update(user_params)
-      if @user.save
-        redirect_to staff_users_path
-        flash[:notice] = "user successfully updated."
-      else
-        render :edit
-        flash[:notice] = "user could not be updated."
-      end
-    end
-
-    def show
-      @user = user.find(params[:id])
-    end
-
-    private
-
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :role)
-    end
-
-    def set_user
-      @user = user.find(params[:id])
+  def create
+    @user = user.new(user_params.merge({password: "12345678", password_confirmation: "12345678"}))
+    if @user.save
+      redirect_to staff_users_path, notice: "user successfully saved."
+    else
+      render :new, alert: "user could not be saved."
     end
   end
 
+  def edit
+    @user = user.find(params[:id])
+  end
+
+  def update
+    @user = user.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to staff_users_path
+      flash[:notice] = "user successfully updated."
+    else
+      render :edit
+      flash[:notice] = "user could not be updated."
+    end
+  end
+
+  def show
+    @user = user.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :role)
+  end
+
+  def set_user
+    @user = user.find(params[:id])
+  end
 end
