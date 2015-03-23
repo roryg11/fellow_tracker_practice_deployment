6012bc085f23d241c5df254e6b2cad71b6febe05
bstil22 <bradydynamite@hotmail.com>
2015-03-23 11:54:05 -0700

    only fellows without cohort can be added

--- app/models/fellow.rb
+++ app/models/fellow.rb
@@ -2,4 +2,7 @@ class Fellow < User
     def full_name
         "#{first_name} #{last_name}"
     end
+    def self.avail_fellows
+      @avail_fellows = Fellow.select { |fellow| fellow.cohorts.length == 0 }
+    end
 end
