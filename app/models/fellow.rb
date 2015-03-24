3e36862a7f551eeae2c8c2ef52b90c678e47afee
roryg11 <Rory.C.Grant@gmail.com>
2015-03-23 18:16:18 -0700

    adding cohorts, fellows and progress to coach Dashboard

--- app/models/fellow.rb
+++ app/models/fellow.rb
@@ -5,4 +5,14 @@ class Fellow < User
     def self.avail_fellows
       @avail_fellows = Fellow.select { |fellow| fellow.cohorts.length == 0 }
     end
+
+    def week_progress
+      monday = Date.today.beginning_of_week
+      sunday = monday + 6
+      (((self.goals.where(completed: true, due_date: monday..sunday ).length).to_f/(self.goals.length).to_f) * 100).floor
+    end
+
+    def overall_progress
+      (((self.goals.where(completed: true).length).to_f/(self.goals.length).to_f) * 100).floor
+    end
 end
