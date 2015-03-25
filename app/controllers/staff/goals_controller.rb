b39a5a23649b244f061c2e2d1509bc7a2f450c86
roryg11 <Rory.C.Grant@gmail.com>
2015-03-25 11:51:59 -0700

    adding info to/styling show and index pages

--- app/controllers/staff/goals_controller.rb
+++ app/controllers/staff/goals_controller.rb
@@ -5,7 +5,26 @@ class Staff::GoalsController < ApplicationController
   end
 
   def index
-    @goals = Goal.all
+    @goals = @fellow.goals
+    if @fellow.memberships.any?
+      @goals = @fellow.goals
+      @cohort_start_date = @fellow.cohorts[0].start_date
+      @week_number = 1
+      @first_monday = @fellow.cohorts[0].first_monday
+      @cohort_phase_array = []
+      weeks_since_launch = ((Date.today - @first_monday)/7).ceil.to_i
+      weeks_since_launch.times do
+        week_hash = {}
+        week_hash[:week_number] = @week_number
+        week_hash[:start] = (@first_monday + (7*(@week_number -1))).strftime("%B %d %Y")
+        week_hash[:end] = (@first_monday + (7*(@week_number -1)) + 6).strftime("%B %d %Y")
+        week_hash[:goals] = []
+        week_hash[:goals] = @goals.where(due_date: week_hash[:start]..week_hash[:end])
+        @cohort_phase_array.push(week_hash)
+        @week_number+=1
+       end
+     @cohort_phase_array
+   end
   end
 
   def new
