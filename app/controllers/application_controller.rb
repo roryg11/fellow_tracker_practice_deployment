cf83cb7cffb00b52aaf5bb16a8db364af203002f
bstil22 <bradydynamite@hotmail.com>
2015-03-28 22:53:55 -0700

    user should be able to edit their name in the system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   protected
 
   def after_sign_in_path_for(user)
@@ -12,13 +14,18 @@ class ApplicationController < ActionController::Base
    elsif user.role == "Coach"
      staff_coach_path(user)
    end
-  end
+ end
 
-  def authorize_staff!
-    if current_user.role != 'Staff' || current_user.role != 'Coach'
-      redirect_to goals
+ def authorize_staff!
+  if current_user.role != 'Staff' || current_user.role != 'Coach'
+    redirect_to goals
       # send to 404 message
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password) }
   end
 
 end
