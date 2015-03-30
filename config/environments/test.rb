989139ffc5ad5455f41a8783a2ee31cc43fd7046
bstil22 <bradydynamite@hotmail.com>
2015-03-30 13:54:26 -0700

    added mailer settings to production and test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,15 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.sendgrid.net',
+  port:                 587,
+  domain:               'uncollege.org',
+  user_name:            ENV["SENDGRID_USERNAME"],
+  password:             ENV["SENDGRID_PASSWORD"],
+  authentication:       'plain',
+  enable_starttls_auto: true, 
+   }
 end
