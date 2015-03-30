4a0b4dab1646d11d34a3bf35a8f689b5cc775c2c
roryg11 <Rory.C.Grant@gmail.com>
2015-03-30 12:05:40 -0700

    adding coaches/fellow progress to coaches index

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -9,4 +9,30 @@ class Coach < User
   def self.not_members_of(cohort)
     Coach.select {|c| !c.cohorts.include?(cohort) }
   end
+
+  def overall_fellow_progress
+    aggregate_fellow_progress = 0
+    num_fellows = fellows.length
+    all_fellow_progress = 0
+    fellows.each do |fellow|
+      aggregate_fellow_progress += fellow.overall_progress
+    end
+    if num_fellows > 0
+      all_fellow_progress = aggregate_fellow_progress/num_fellows
+    end
+    all_fellow_progress
+  end
+
+  def weekly_fellow_progress
+    aggregate_fellow_progress = 0
+    num_fellows = fellows.length
+    all_fellow_progress = 0
+    fellows.each do |fellow|
+      aggregate_fellow_progress += fellow.week_progress
+    end
+    if num_fellows > 0
+      all_fellow_progress = aggregate_fellow_progress/num_fellows
+    end
+    all_fellow_progress
+  end
 end
