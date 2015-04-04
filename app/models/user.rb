206322e16f3bad1cdb29a67929f8733ef2c217d0
roryg11 <Rory.C.Grant@gmail.com>
2015-04-04 14:59:58 -0700

    when a fellow is deleted so are their goals

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :goals
+  has_many :goals, :dependent => :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :cohorts, through: :memberships
